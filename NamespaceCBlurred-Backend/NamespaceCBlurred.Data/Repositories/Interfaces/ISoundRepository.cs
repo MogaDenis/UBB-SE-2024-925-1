ca5a0d5650a3286c03580ffc21c06b512f46ba02
MogaDenis <moga_denis@yahoo.com>
2024-05-11 15:17:53 +0300

    Created Repository for Sounds

---
+++ NamespaceCBlurred-Backend/NamespaceCBlurred.Data/Repositories/Interfaces/ISoundRepository.cs
@@ -0,0 +1,13 @@
+﻿using NamespaceCBlurred.Data.Models;
+
+namespace NamespaceCBlurred.Data.Repositories.Interfaces
+{
+    public interface ISoundRepository
+    {
+        Task<Sound?> GetSoundById(int soundId);
+        Task<IEnumerable<Sound>> GetAllSounds();
+        Task<int> AddSound(Sound sound);
+        Task<bool> DeleteSound(int soundId);
+        Task<bool> UpdateSound(int soundId, Sound sound);   
+    }
+}
---
+++ NamespaceCBlurred-Backend/NamespaceCBlurred.Data/Repositories/SoundRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NamespaceCBlurred.Data.Models;
+using NamespaceCBlurred.Data.Repositories.Interfaces;
+
+namespace NamespaceCBlurred.Data.Repositories
+{
+    public class SoundRepository : ISoundRepository
+    {
+        private readonly NamespaceCBlurredContext _context;
+
+        public SoundRepository(NamespaceCBlurredContext context) 
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<int> AddSound(Sound sound)
+        {
+            await _context.Sounds.AddAsync(sound);
+            await _context.SaveChangesAsync();
+
+            return sound.Id;
+        }
+
+        public async Task<bool> DeleteSound(int soundId)
+        {
+            var soundToRemove = await _context.Sounds.FirstOrDefaultAsync(sound => sound.Id == soundId);
+            if (soundToRemove == null) 
+            {
+                return false;
+            }
+
+            _context.Sounds.Remove(soundToRemove);
+            return true;
+        }
+
+        public async Task<IEnumerable<Sound>> GetAllSounds()
+        {
+            return await _context.Sounds.ToListAsync();
+        }
+
+        public async Task<Sound?> GetSoundById(int soundId)
+        {
+            return await _context.Sounds.FirstOrDefaultAsync(sound => sound.Id == soundId);
+        }
+
+        public async Task<bool> UpdateSound(int soundId, Sound sound)
+        {
+            var soundToUpdate = await _context.Sounds.FirstOrDefaultAsync(sound => sound.Id == soundId);
+            if (soundToUpdate == null)
+            {
+                return false;
+            }
+
+            sound.Id = soundId;
+
+            _context.Sounds.Entry(soundToUpdate).CurrentValues.SetValues(sound);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
