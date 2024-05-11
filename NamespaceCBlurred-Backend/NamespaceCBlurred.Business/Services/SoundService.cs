0aeaf3bbdbe9f1e98c58f5bf94cffea1c2258412
MogaDenis <moga_denis@yahoo.com>
2024-05-11 16:39:17 +0300

    Added sound service and mapper

---
+++ NamespaceCBlurred-Backend/NamespaceCBlurred.Business/Services/SoundService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using NamespaceCBlurred.Business.Models;
+using NamespaceCBlurred.Business.Services.Interfaces;
+using NamespaceCBlurred.Data.Models;
+using NamespaceCBlurred.Data.Repositories.Interfaces;
+
+namespace NamespaceCBlurred.Business.Services
+{
+    public class SoundService : ISoundService
+    {
+        private readonly ISoundRepository _soundRepository;
+        private readonly IMapper _mapper;
+
+        public SoundService(ISoundRepository soundRepository, IMapper mapper)
+        {
+            _soundRepository = soundRepository ?? throw new ArgumentNullException(nameof(soundRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<int> AddSound(SoundForAddUpdateModel soundModel)
+        {
+            return await _soundRepository.AddSound(_mapper.Map<Sound>(soundModel));
+        }
+
+        public async Task<bool> DeleteSound(int soundId)
+        {
+            return await _soundRepository.DeleteSound(soundId);
+        }
+
+        public Task<IEnumerable<Sound>> GetAllSounds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Sound?> GetSoundById(int soundId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateSound(int soundId, SoundForAddUpdateModel soundModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
