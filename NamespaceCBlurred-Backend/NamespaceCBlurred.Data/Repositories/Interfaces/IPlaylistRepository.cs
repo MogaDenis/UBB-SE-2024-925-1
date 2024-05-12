4a147da89235fb6cb1c0c45f95586a4174d2a4e0
MogaDenis <moga_denis@yahoo.com>
2024-05-12 13:59:51 +0300

    Started work on Playlist entity

---
+++ NamespaceCBlurred-Backend/NamespaceCBlurred.Data/Repositories/Interfaces/IPlaylistRepository.cs
@@ -0,0 +1,14 @@
+﻿using NamespaceCBlurred.Data.Models;
+
+namespace NamespaceCBlurred.Data.Repositories.Interfaces
+{
+    public interface IPlaylistRepository
+    {
+        Task<Playlist?> GetPlaylistById(int playlistId);
+        Task<IEnumerable<Playlist>> GetAllPlaylists();
+        Task<IEnumerable<Playlist>> GetAllPlaylistsOfUser(int userId);
+        Task<int> AddPlaylist(Playlist song);
+        Task<bool> DeletePlaylist(int songId);
+        Task<bool> UpdatePlaylist(int songId, Playlist song);
+    }
+}
